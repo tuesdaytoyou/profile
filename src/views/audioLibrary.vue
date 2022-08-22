e8472780a3abdedd6c2e35dba3e73a332168a8d7
tuesdaytoyou <tuesdaytoyou@icloud.com>
2022-08-23 00:22:52 +0800

    deploy

--- src/views/audioLibrary.vue
+++ src/views/audioLibrary.vue
@@ -6,17 +6,13 @@
           class="audio_list flex_column" :class="{ 'audio_cur': audio.cur }"><span>{{ audio.name }}</span></li>
       </ul>
     </div>
-    <div class="flex_column" style="position:absolute;top:5740px;background-color:black;width:100%;height:1362px;">
-      <img v-if="curIndex == 1" :src="getCloudUrl('audio_list1.png')" />
-      <iframe v-if="curIndex == 2" style="border: 1px solid rgba(0, 0, 0, 0.1);" width="1600" height="1362"
+    <div class="flex_column" style="position:absolute;top:5900px;background-color:black;width:100%;height:1080px;">
+      <img v-show="curIndex == 1" :src="getCloudUrl('audio_list1.png')" />
+      <iframe v-show="curIndex == 2" style="border: 1px solid rgba(0, 0, 0, 0.1);" width="1600" height="1362"
         src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F9C4FkOastcC6AIpjh8PkuA%2F%25E9%25A1%25B9%25E7%259B%25AE%25E6%2595%25B4%25E7%2590%2586%3Fnode-id%3D141%253A40848%26scaling%3Dcontain%26page-id%3D141%253A33439%26starting-point-node-id%3D141%253A40848%26show-proto-sidebar%3D1"
         allowfullscreen></iframe>
-      <!-- <iframe v-if="curIndex == 2" style="border: 1px solid rgba(0, 0, 0, 0.1);z-index:100;transform: scale(0.47, 0.47)"
-        width="1920" height="1080"
-        src="https://www.figma.com/file/9C4FkOastcC6AIpjh8PkuA/%E9%A1%B9%E7%9B%AE%E6%95%B4%E7%90%86?node-id=141%3A33439"
-        allowfullscreen></iframe> -->
-      <img v-if="curIndex == 3" :src="getCloudUrl('audio_list3.png')" />
-      <img v-if="curIndex == 4" :src="getCloudUrl('audio_list4.png')" />
+      <img v-show="curIndex == 3" :src="getCloudUrl('audio_list3.png')" />
+      <img v-show="curIndex == 4" :src="getCloudUrl('audio_list4.png')" />
     </div>
     <div class="audio_imgs flex_column">
       <img v-for="index in 2" :src="getCloudUrl('audio_img' + index + '.png')" alt="" />
@@ -24,12 +20,13 @@
   </div>
 </template>
 <script>
-import { defineComponent, getCurrentInstance, reactive, ref } from "vue";
-
+import { defineComponent, getCurrentInstance, reactive, ref, onMounted } from "vue";
+import useMenuChange from "../hooks/useMenuChange"
 
 export default defineComponent({
   name: "audio",
   setup() {
+    useMenuChange()
     const internalInstance = getCurrentInstance()
     const $utils = internalInstance.appContext.config.globalProperties.$utils
     const getCloudUrl = (url) => {
