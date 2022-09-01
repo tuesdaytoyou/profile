b204159c46f047d8f41d5c480de0f5401a0048bd
jialongwei <jialongwei@fang.com>
2022-09-01 10:02:15 +0800

    blogCardList

--- src/views/blogBox.vue
+++ src/views/blogBox.vue
@@ -36,14 +36,14 @@ export default defineComponent({
     const internalInstance = getCurrentInstance()
     const $utils = internalInstance.appContext.config.globalProperties.$utils
     const blogCardList = [
-      // {
-      //   title: "MR Design System",
-      //   description:
-      //     "随着元宇宙到来，MR混合现实的设计系统存在什么样的特点？通过研究世界先进企业的探索，并进行总结。",
-      //   keys: ["设计系统", "VR、AR、MR"],
-      //   img: 'blog_img1.png',
-      //   url: 'https://ludicrous-harmony-bcc.notion.site/MR-Design-System-e9861a5358f4461d9d53e1d175a00525'
-      // },
+      {
+        title: "用户体验走查方法论总结与思考",
+        description:
+          "在喜马拉雅实习期间，通过体验指标，进行体验走查项目，发掘体验问题，为产品体验改进提供方向。",
+        keys: ["企业实习", "体验走查"],
+        img: 'blog_img2.png',
+        url: 'https://ludicrous-harmony-bcc.notion.site/677fd60d273a46b5812d3f3c23072405'
+      },
       {
         title: "如何组织设计Workshop助力设计规范研究",
         description:
@@ -53,12 +53,12 @@ export default defineComponent({
         url: 'https://ludicrous-harmony-bcc.notion.site/Workshop-c086347e638e46b282abb6de593244ff'
       },
       {
-        title: "用户体验走查方法论总结与思考",
+        title: "MR Design System",
         description:
-          "在喜马拉雅实习期间，通过体验指标，进行体验走查项目，发掘体验问题，为产品体验改进提供方向。",
-        keys: ["企业实习", "体验走查"],
-        img: 'blog_img2.png',
-        url: 'https://ludicrous-harmony-bcc.notion.site/677fd60d273a46b5812d3f3c23072405'
+          "随着元宇宙到来，MR混合现实的设计系统存在什么样的特点？通过研究世界先进企业的探索，并进行总结。",
+        keys: ["设计系统", "VR、AR、MR"],
+        img: 'blog_img1.png',
+        url: 'https://ludicrous-harmony-bcc.notion.site/MR-Design-System-e9861a5358f4461d9d53e1d175a00525'
       },
       // {
       //   title: "企业C端产品交互设计规范制定",
